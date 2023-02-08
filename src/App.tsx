b4f39812d709454941359e3d65bf10f1f5de3eac
Wilson <wilsonla0920@gmail.com>
2023-02-07 20:17:00 -0500

    Implemented carousel component for desktop and mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './App.css';
-import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import React from "react";
+import "./App.css";
+import { Carousel } from "./layouts/HomePage/Carousel";
+import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel />
     </div>
   );
 }
