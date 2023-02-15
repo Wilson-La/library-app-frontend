ddd6176a128e6241ba59cb15c6b076c5a00cca86
Wilson <wilsonla0920@gmail.com>
2023-02-15 18:44:39 -0500

    Implemented SearchBook component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </div>
   );
