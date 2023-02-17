e475fc8da75aeddb43115a983d67dc08d898fa99
Wilson <wilsonla0920@gmail.com>
2023-02-17 17:59:12 -0500

    Implemented CheckoutAndReviewBox component and added it to our BookCheckoutPage component

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import BookModel from '../../models/BookModel';
 import { SpinnerLoading } from '../Utils/SpinnerLoading';
 import { StarsReview } from '../Utils/StarsReview';
+import { CheckoutAndReviewBox } from './CheckoutAndReviewBox';
 
 export const BookCheckoutPage = () => {
   const [book, setBook] = useState<BookModel>();
@@ -78,6 +79,7 @@ export const BookCheckoutPage = () => {
               <StarsReview rating={4.5} size={32} />
             </div>
           </div>
+          <CheckoutAndReviewBox book={book} mobile={false} />
         </div>
         <hr />
       </div>
@@ -102,6 +104,7 @@ export const BookCheckoutPage = () => {
             <StarsReview rating={4.5} size={32} />
           </div>
         </div>
+        <CheckoutAndReviewBox book={book} mobile={true} />
         <hr />
       </div>
     </div>
