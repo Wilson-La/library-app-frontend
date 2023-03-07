4a573f004c0a033c0da5e901be00803135500b6b
Wilson <wilsonla0920@gmail.com>
2023-03-07 15:32:17 -0500

    Implemented a change to CheckoutAndReviewBox so that when a user is loggedin it will show Checkout or a message that shows if the book is already checked out instead of a sign in button

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -209,6 +209,8 @@ export const BookCheckoutPage = () => {
             book={book}
             mobile={false}
             currentLoansCount={currentLoansCount}
+            isAuthenticated={authState?.isAuthenticated}
+            isCheckedOut={isCheckedOut}
           />
         </div>
         <hr />
@@ -239,6 +241,8 @@ export const BookCheckoutPage = () => {
           book={book}
           mobile={true}
           currentLoansCount={currentLoansCount}
+          isAuthenticated={authState?.isAuthenticated}
+          isCheckedOut={isCheckedOut}
         />
         <hr />
         <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
