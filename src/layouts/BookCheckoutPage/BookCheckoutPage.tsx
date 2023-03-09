901a6079c4f2d492936fa7c6d05ecfb945db218b
Wilson <wilsonla0920@gmail.com>
2023-03-09 17:33:02 -0500

    Implemented useEffect for user review in BookCheckoutPage

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -111,11 +111,29 @@ export const BookCheckoutPage = () => {
       setIsLoadingReview(false);
       setHttpError(error.message);
     });
-  }, []);
+  }, [isReviewLeft]);
 
   //useEffect for user review
   useEffect(() => {
-    const fetchUserReviewBook = async () => {};
+    const fetchUserReviewBook = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/reviews/secure/user/book/?bookId=${bookId}`;
+        const requestOptions = {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        };
+        const userReview = await fetch(url, requestOptions);
+        if (userReview.ok) {
+          throw new Error('Something went wrong!');
+        }
+        const userReviewResponseJson = await userReview.json();
+        setIsReviewLeft(userReviewResponseJson);
+      }
+      setIsLoadingUserReview(false);
+    };
     fetchUserReviewBook().catch((error: any) => {
       setIsLoadingUserReview(false);
       setHttpError(error.message);
@@ -182,7 +200,8 @@ export const BookCheckoutPage = () => {
     isLoading ||
     isLoadingReview ||
     isLoadingCurrentLoansCount ||
-    isLoadingBookCheckedOut
+    isLoadingBookCheckedOut ||
+    isLoadingUserReview
   ) {
     return <SpinnerLoading />;
   }
