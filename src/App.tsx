57dd913d633b5ffd6613e6dd6c686c8b0a059526
Wilson <wilsonla0920@gmail.com>
2023-03-13 16:40:33 -0400

    Added a SecureRoute for user to get the /shelf and also created a NavLink for it on the navbar if user is authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 import { oktaConfig } from './lib/oktaConfig';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
-import { Security, LoginCallback } from '@okta/okta-react';
+import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/ReviewListPage/ReviewListPage';
+import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -55,6 +56,9 @@ export const App = () => {
               render={() => <LoginWidget config={oktaConfig} />}
             />
             <Route path='/login/callback' component={LoginCallback} />
+            <SecureRoute path='/shelf'>
+              <ShelfPage />
+            </SecureRoute>
           </Switch>
         </div>
         <Footer />
