41e39e63a1a05f058e4766fd3a3574c6b6cf9b26
Wilson <wilsonla0920@gmail.com>
2023-03-17 18:46:04 -0400

    Implemented ManageLibraryPage for admin users to use to add books, change quantities, and messages

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/ReviewListPage/ReviewListPage';
 import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
+import { ManageLibraryPage } from './layouts/ManageLibraryPage/ManageLibraryPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -62,6 +63,9 @@ export const App = () => {
             </SecureRoute>
             <SecureRoute path='/messages'>
               <MessagesPage />
+            </SecureRoute>
+            <SecureRoute path='/admin'>
+              <ManageLibraryPage />
             </SecureRoute>
           </Switch>
         </div>
